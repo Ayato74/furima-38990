f399624591f75d907d960150cc5f0206650329b1
Ayato74 <uminomori74@gmail.com>
2023-07-19 16:01:14 +0900

    商品情報編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_edit, only: [:edit, :update]
 
   def index
@@ -21,16 +22,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
     @user = @item.user
   end
 
   def edit
-    @item = Item.find(params[:id])
+
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -40,12 +39,15 @@ class ItemsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:title, :explanation, :category_id, :condition_id, :delivery_charge_id, :region_id, :num_of_day_id, :price, :image).merge(user_id: current_user.id)
   end
 
   def move_to_edit
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user == @item.user
       redirect_to root_path
     end
